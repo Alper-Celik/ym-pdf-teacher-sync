5f486955ff030e9266830aefe14b28fe005ab724
Alper Çelik <alper@alper-celik.dev>
2025-10-28 15:35:50 +0300

    flush when logging

--- log.py
+++ log.py
@@ -4,4 +4,4 @@ yellow_code = "[33m"
 
 def log(message, color_escape=term_escape + yellow_code):
     color_reset_escape = term_escape + "[0m"
-    print(color_escape + message + color_reset_escape)
+    print(color_escape + message + color_reset_escape, flush=True)
